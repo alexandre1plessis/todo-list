1aa6f844faab91fd5b7414ea06a38906a72af5fd
NatachaEt <natacha.ethievant@unsolite.com>
2023-03-03 10:53:26 +0100

    Ajout lists

--- apps/app/src/pages/HomePage.vue
+++ apps/app/src/pages/HomePage.vue
@@ -1,21 +1,32 @@
 <template>
   <h1>Home</h1>
     <CardList
-      v-for="list in listStore.lists"
+      v-for="list in lists"
       :key="list.id"
       :title="list.title"
     ></CardList>
   <button>+</button>
+  <label>Nom de la nouvelle lists</label>
+  <input v-model="titre" placeholder="edit me">
+  <button @click="save()">save</button>
 </template>
 <script setup>
 import CardList from 'components/CardList.vue'
 import { useListStore } from 'stores/list-store'
-import { onMounted } from 'vue'
+import { onMounted, computed, ref } from 'vue'
 
+const titre = ref()
 const listStore = useListStore()
-listStore.getLists()
+const lists = computed(() => listStore.lists)
 
-onMounted(() => {
+function save () {
+  if (titre.value) {
+    listStore.addList(titre.value)
+  }
+}
+
+onMounted(async () => {
+  await listStore.getLists()
 })
 </script>
 <style scoped>
