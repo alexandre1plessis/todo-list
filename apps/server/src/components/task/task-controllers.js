ae2d61cb074e63334f220db55508f49521d35006
alexandre <alexandre1.plessis@gmail.com>
2023-03-03 11:01:01 +0100

    Updated api, added checks and ability to move a task

--- apps/server/src/components/task/task-controllers.js
+++ apps/server/src/components/task/task-controllers.js
@@ -23,6 +23,8 @@ export const getOneTask = async (ctx) => {
 
 export const createTask = async (ctx) => {
   try {
+    if (!ctx.request.body.title) throw new Error('Title is missing')
+
     const schema = joi.object({
       title: joi.string().required(),
       description: joi.string(),
@@ -41,6 +43,8 @@ export const createTask = async (ctx) => {
 
 export const updateTask = async (ctx) => {
   try {
+    if (!ctx.request.body.title) throw new Error('Title is missing')
+
     const schema = joi.object({
       title: joi.string(),
       description: joi.string(),
@@ -66,3 +70,41 @@ export const deleteTask = async (ctx) => {
     ctx.badRequest({ message: error.message })
   }
 }
+
+export const addTaskToListById = async (ctx) => {
+  try {
+    const schema = joi.object({
+      taskId: joi.string().required(),
+      task: joi.object({
+        title: joi.string().required(),
+        description: joi.string(),
+        state: joi.boolean()
+      }).required()
+    })
+    const { error, value } = schema.validate(ctx.request.body)
+
+    if (error) throw new Error(error)
+
+    const list = await ListModel.findById(ctx.params.listId)
+    if (!list) throw new Error('List not found')
+
+    const task = await TaskModel.findById(value.taskId)
+    if (!task) throw new Error('Task not found')
+
+    const existingTaskIndex = list.tasks.findIndex(existingTask => existingTask._id.equals(task._id))
+    if (existingTaskIndex >= 0) throw new Error('Task already exists in list')
+
+    // Remove the task from any other lists where it might have existed
+    const otherLists = await ListModel.find({ 'tasks._id': task._id })
+    const otherListIds = otherLists.map(otherList => otherList._id)
+    await ListModel.updateMany({ _id: { $in: otherListIds } }, { $pull: { tasks: { _id: task._id } } })
+
+    // Add the task to the new list
+    list.tasks.push(value.task)
+    const updatedList = await list.save()
+    ctx.ok(updatedList)
+  } catch (error) {
+    ctx.badRequest({ message: error.message })
+  }
+}
+
