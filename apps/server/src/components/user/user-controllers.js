0fc0a5f52e82a0716edf13d780ebeea29a2ac0f2
alexandre <alexandre1.plessis@gmail.com>
2023-05-04 11:35:53 +0200

    Modificationinscription for password

--- apps/server/src/components/user/user-controllers.js
+++ apps/server/src/components/user/user-controllers.js
@@ -5,26 +5,39 @@ import jwt from 'jsonwebtoken'
 
 // Fonction pour créer un compte utilisateur
 export const createUser = async (ctx) => {
-    try {
-      const schema = joi.object({
-        name: joi.string().required(),
-        email: joi.string().email().required(),
-        password: joi.string().required()
-      })
-      const { error, value } = schema.validate(ctx.request.body)
-  
-      if(error) throw new Error(error)
-  
-      //On vérifie si l'email existe déjà
-      const existingUser = await UserModel.findOne({ email: value.email })
-      if(existingUser) throw new Error('Email already exists')
-  
-      const user = await UserModel.create(value)
-      ctx.ok(user)
-    } catch (error) {
-      ctx.badRequest({ message: error.message })
-    }
+  try {
+    const schema = joi.object({
+      name: joi.string().required(),
+      email: joi.string().email().required(),
+      password: joi.string().required()
+    });
+    const { error, value } = schema.validate(ctx.request.body);
+
+    if(error) throw new Error(error);
+
+    // On vérifie si l'email existe déjà
+    const existingUser = await UserModel.findOne({ email: value.email });
+    if(existingUser) throw new Error('Email already exists');
+
+    // On crypte le mot de passe
+    const hashedPassword = await bcrypt.hash(value.password, 10);
+
+    const user = await UserModel.create({
+      name: value.name,
+      email: value.email,
+      password: hashedPassword
+    });
+    
+    // On retourne les informations de l'utilisateur, en excluant le mot de passe
+    ctx.ok({
+      id: user._id,
+      name: user.name,
+      email: user.email
+    });
+  } catch (error) {
+    ctx.badRequest({ message: error.message });
   }
+};
   
   export const authenticateUser = async (ctx) => {
     try {
