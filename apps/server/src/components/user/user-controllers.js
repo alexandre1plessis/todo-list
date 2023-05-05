e6ff25d6b2875fc52c2cbeb55b9b764331a098f6
alexandre <alexandre1.plessis@gmail.com>
2023-05-05 12:09:33 +0200

    Modified connection for user

--- apps/server/src/components/user/user-controllers.js
+++ apps/server/src/components/user/user-controllers.js
@@ -39,34 +39,38 @@ export const createUser = async (ctx) => {
   }
 };
   
-  export const authenticateUser = async (ctx) => {
-    try {
-      const schema = joi.object({
-        email: joi.string().email().required(),
-        password: joi.string().required()
-      })
-      const { error, value } = schema.validate(ctx.request.body)
-  
-      if(error) throw new Error(error)
-  
-      //On vérifie si l'email existe
-      const user = await UserModel.findOne({ email: value.email })
-      if(!user) throw new Error('Invalid email or password')
-  
-      //On vérifie si le mot de passe est correct
-      const isPasswordValid = await bcrypt.compare(value.password, user.password)
-      if(!isPasswordValid) throw new Error('Invalid email or password')
-  
-      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET)
-        ctx.ok({
-            token,
-            user: {
-                id: user._id,
-                name: user.firstName,
-                email: user.email
-            }
-        })
-    } catch (error) {
-      ctx.badRequest({ message: error.message })
-    }
+export const authenticateUser = async (ctx) => {
+  try {
+    const schema = joi.object({
+      email: joi.string().email().required(),
+      password: joi.string().required()
+    });
+    const { error, value } = schema.validate(ctx.request.body);
+
+    if (error) throw new Error(error);
+
+    const user = await UserModel.findOne({ email: value.email });
+    if (!user) throw new Error('Invalid email or password');
+
+    const isPasswordValid = await bcrypt.compare(value.password, user.password);
+    if (!isPasswordValid) throw new Error('Invalid email or password');
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+    const userId = decodedToken.id;
+    const authenticatedUser = await UserModel.findById(userId).select('-password');
+
+    ctx.state.user = authenticatedUser;
+    ctx.ok({
+      token,
+      user: {
+        id: authenticatedUser._id,
+        name: authenticatedUser.firstName,
+        email: authenticatedUser.email
+      }
+    });
+  } catch (error) {
+    ctx.badRequest({ message: error.message });
   }
+};
